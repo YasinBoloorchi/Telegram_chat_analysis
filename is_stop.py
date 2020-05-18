7e31671ded1d608d553a72fef4e5ecd338181bd3
hakim <yasin.b76@gmail.com>
2020-05-18 11:34:36 +0430

    Create is_stop program that will communicate with tokenizer program for checking the stop words

---
+++ is_stop.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3.8
+
+import socket
+import pickle
+from datetime import datetime
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 9876
+
+# startup settings
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+BUFFER_SIZE = 1000
+
+stop_words = ['این', 'و', 'در', 'با', 'اون', 'برای', 'از', 'که', 'هر']
+def is_stop_word(word):
+    if word in stop_words:
+        print(f'{word} IS STOP WORD')
+        return True
+    
+    else:
+        return False
+
+
+while True:
+    print('stop word checker is listening..')
+    tokenizer_socket, tokenizer_address = server_socket.accept()
+    print(f"Connection from {tokenizer_address} has been stablished!")
+
+    while True:
+        msg = tokenizer_socket.recv(100)
+        
+        data = pickle.loads(msg)
+        
+        if data == 'END':
+            break
+
+        # print(type(data), data)
+
+        res = is_stop_word(data)
+
+        tokenizer_socket.send(pickle.dumps(res))
+
